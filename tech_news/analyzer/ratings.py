920d2e5781691ccde594ea224a3658ee697734ed
“brunobatista” <brunobatista101@gmail.com>
2020-11-26 19:47:12 -0300

    teste

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,26 @@
+# flake8: noqa
+
+from tech_news.database import aggregate_news
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    query = [{"$project": {"url": 1, "title": 1, "orderBySum": {
+        "$add": ["$shares_count", "$comments_count"]}}},
+        {"$sort": {"orderBySum": -1, "title": 1}}, {"$limit": 5}]
+    news = aggregate_news(query)
+    news_list = []
+    for each in news:
+        new = (each["title"], each["url"])
+        news_list.append(new)
+    return news_list
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    query = [{"$unwind": "$categories"}, {"$group": {"_id": "$categories",  "frequency": {"$sum": 1}}},
+             {"$sort": {"frequency": -1, "_id": 1}},
+             {"$limit": 5}]
+    news = aggregate_news(query)
+    news_list = []
+    for each in news:
+        new = (each["_id"])
+        news_list.append(new)
+    return news_list

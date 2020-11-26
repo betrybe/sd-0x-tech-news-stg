0e44576dc97d5fb20cb40192187878d04c8e233b
bruno batista <brunobatista101@gmail.com>
2020-11-26 20:45:04 -0300

    Update test_ratings.py

--- tests/test_analyzer/test_ratings.py
+++ tests/test_analyzer/test_ratings.py
@@ -6,7 +6,7 @@ from decouple import config
 DB_HOST = config("DB_HOST", default="localhost")
 DB_PORT = config("DB_PORT", default="27017")
 
-client = MongoClient(host=DB_HOST, port=int(DB_PORT), connect=False))
+client = MongoClient(host=DB_HOST, port=int(DB_PORT), connect=False)
 
 db = client.tech_news
 

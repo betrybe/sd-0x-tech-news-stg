5afc681942449f7654e49970063d2d6a9fec5d56
bruno batista <brunobatista101@gmail.com>
2020-11-26 20:31:30 -0300

    Update database.py

--- tech_news/database.py
+++ tech_news/database.py
@@ -7,7 +7,7 @@ from decouple import config
 DB_HOST = config("DB_HOST", default="localhost")
 DB_PORT = config("DB_PORT", default=27017)
 
-client = MongoClient(host=DB_HOST, port=int(DB_PORT))
+client = MongoClient(host=DB_HOST, port=int(DB_PORT), connect=False))
 
 db = client.tech_news
 

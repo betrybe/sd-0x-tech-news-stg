b7fee527dad849d11124caee645550d2120c3da7
“brunobatista” <brunobatista101@gmail.com>
2020-11-26 20:51:49 -0300

    teste

--- tech_news/database.py
+++ tech_news/database.py
@@ -2,9 +2,10 @@ from pymongo import MongoClient
 from decouple import config
 
 DB_HOST = config("DB_HOST", default="localhost")
-DB_PORT = config("DB_PORT", default="27017")
+DB_PORT = config("DB_PORT", default=27017)
 
-client = MongoClient(host=DB_HOST, port=int(DB_PORT))
+client = MongoClient(host=DB_HOST, port=int(DB_PORT), connect=False)
+
 db = client.tech_news
 
 
